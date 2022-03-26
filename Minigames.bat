0718488a2ba36b8acaf93ff1d29c6e6a945ea01b
Jonahgus <102358877+Jonahgus@users.noreply.github.com>
2022-03-26 10:56:36 +0100

    Add files via upload

--- Minigames.bat
+++ Minigames.bat
@@ -4,6 +4,7 @@ title Fun minigames
 set /A cookies=0
 
 :startmenu
+cls
 timeout /NOBREAK 1
 echo _______________________________________________
 if %cookies%==1 echo you ate %cookies% cookie :D Yummy!
@@ -15,9 +16,10 @@ if %cookies%==6 echo you ate %cookies% cookies :D Chill
 if %cookies%==7 echo you ate %cookies% cookies :D Bro no
 if %cookies%==8 echo you ate %cookies% cookies :D You'll get fat
 if %cookies%==9 echo you ate %cookies% cookies :D I'm warning you
-if %cookies%==10 start cmd.exe /k %~dp0\full.bat
+if %cookies%==10 start "%~dp0\full.bat"
 echo ________________________________________________
-timeout /NOBREAK 3
+timeout /NOBREAK 2
+cls
 echo ________________________________________________________
 echo Welcome to Jonahgus's minigames
 echo have fun!
@@ -37,6 +39,7 @@ if %startinput%==2 GOTO :minigame2
 if %startinput%==3 GOTO :cookie
 
 :cookie
+cls
 set /A cookies=%cookies%+1
 echo ________________________
 echo yummy
@@ -47,12 +50,14 @@ GOTO :startmenu
 
 :minigame1
 
-
+cls 
 set /a wins=0
 :start
+cls
 set /a points=0
 if %wins%==10 GOTO :supercool
 :secretstart
+cls
 echo Hi!
 echo You have %wins% wins so far
 echo Do you want to play easy mode or hard mode?
@@ -60,7 +65,7 @@ echo 1 = Easy mode
 echo 2 = Medium mode
 echo 3 = Hard mode
 echo 4 To go to the home menu
-set /p input="1, 2 or 3: "
+set /p input="1, 2, 3 or 4: "
 if %input%==1 GOTO :easy
 if %input%==2 GOTO :medium
 if %input%==3 GOTO :hard
@@ -68,67 +73,83 @@ if %input%==4 GOTO :startmenu
 if %input%==69 GOTO :secret
 if %input%==10 GOTO :test
 
+echo error try again!
+timeout 2
+GOTO :start
+
 :yeet1
+cls
 set /a points=%points%-1
 echo ________________________________________________________
 echo Thats the wrong answer :(
 echo You now have %points% points
 echo ________________________________________________________
+timeout 2
 if %points%==-5 GOTO :Loser
 if %points%==99 GOTO :coolguy
 if %points%==101 GOTO :coolguy
 GOTO :hard
 
 :yeet1easy
+cls
 set /a points=%points%-1
 echo ________________________________________________________
 echo Thats the wrong answer :(
 echo You now have %points% points
 echo ________________________________________________________
+timeout 2
 if %points%==-5 GOTO :Loser
 if %points%==99 GOTO :coolguy
 if %points%==101 GOTO :coolguy
 GOTO :easy
 
 :yeet1medium
+cls
 set /a points=%points%-1
 echo ________________________________________________________
 echo Thats the wrong answer :(
 echo You now have %points% points
 echo ________________________________________________________
+timeout 2
 if %points%==-5 GOTO :Loser
 if %points%==99 GOTO :coolguy
 if %points%==101 GOTO :coolguy
 GOTO :medium
 
 :yeet
+cls
 set /a points=%points%+1
 echo ________________________________
 echo Correct!
 echo You have %points% points!
 echo ________________________________
+timeout 2
 if %points%==10 GOTO :lol
 if %points%==99 GOTO :coolguy
 if %points%==101 GOTO :coolguy
 GOTO :hard
 
 :yeetmedium
+cls
 set /a points=%points%+1
 echo ________________________________
 echo Correct!
 echo You have %points% points!
 echo ________________________________
+timeout 2
 if %points%==10 GOTO :lol
 if %points%==99 GOTO :coolguy
 if %points%==101 GOTO :coolguy
 GOTO :medium
 
 :yeeteasy
+cls
 set /a points=%points%+1
 echo ________________________________
 echo Correct!
 echo You have %points% points!
 echo ________________________________
+timeout 2
 if %points%==10 GOTO :lol
 if %points%==99 GOTO :coolguy
 if %points%==101 GOTO :coolguy
@@ -136,6 +157,7 @@ GOTO :easy
 
 
 :hard
+cls
 set /A ok=%RANDOM%
 set /p Type="Type the number %ok%: "
 if %ok%==%Type% GOTO :yeet
@@ -143,6 +165,7 @@ if %ok%==%Type% GOTO :yeet
 GOTO :yeet1
 
 :medium
+cls
 set /A ok3=%RANDOM%*100/32768
 set /p Type3="Type the number %ok3%: "
 if %ok3%==%Type3% GOTO :yeetmedium
@@ -150,6 +173,7 @@ if %ok3%==%Type3% GOTO :yeetmedium
 GOTO :yeet1medium
 
 :easy
+cls
 set /A ok2=%RANDOM%*10/32768
 set /p Type2="Type the number %ok2%: "
 if %ok2%==%Type2% GOTO :yeeteasy
@@ -157,18 +181,22 @@ if %ok2%==%Type2% GOTO :yeeteasy
 GOTO :yeet1easy
 
 :lol
+cls
 echo _____________________________
 echo You win!
 echo You get 1 win!
 set /a wins=%wins%+1
 echo _____________________________
+timeout 2
 GOTO :start
 
 :loser
+cls
 echo _____________________________
 echo You lost!
 echo Try again.
 echo _____________________________
+timeout 2
 GOTO :start
 
 :secret
@@ -179,6 +207,7 @@ echo you just got 100 free points bro
 echo nice
 echo anything you choose now you will instantly win :)
 echo _________________________________________________________________
+timeout 2
 GOTO :secretstart 
 
 :cool
@@ -209,6 +238,7 @@ echo y for yes n for no, h for home menu
 echo ________________________
 set /p lool="y, n or h: "
 echo _______________________
+timeout 2
 if %lool%==y GOTO :opnieuw
 if %lool%==n exit
 if %lool%==h GOTO :startmenu
@@ -216,4 +246,4 @@ if %lool%==h GOTO :startmenu
 
 
 :opnieuw
-start cmd /k %~dp0\nolife.bat
+start "%~dp0\nolife.bat"
