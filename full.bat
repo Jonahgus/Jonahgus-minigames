395a8fac60ce99979fe781ddcd747b268063b308
Jonahgus <102358877+Jonahgus@users.noreply.github.com>
2022-05-11 12:56:47 +0200

    Update full.bat

--- full.bat
+++ full.bat
@@ -1,6 +1,5 @@
 md "C:\mini
 echo x=msgbox("Bro ur fat" ,0, "Fat fuck")>"C:\mini\msg.vbs
-start cscript "C:\mini\msg.vbs"
 
 echo set url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 start chrome.exe %url%
