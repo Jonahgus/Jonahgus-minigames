9095dde06e4af3456934086d7be1614c74d6ff92
Jonahgus <102358877+Jonahgus@users.noreply.github.com>
2022-05-17 14:42:19 +0200

    Delete full.bat

--- full.bat
+++
@@ -1,5 +0,0 @@
-md "C:\mini
-echo x=msgbox("Bro ur fat" ,0, "Fat fuck")>"C:\mini\msg.vbs
-start cscript "C:\mini\msg.vbs"
-echo set url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-start chrome.exe %url%
